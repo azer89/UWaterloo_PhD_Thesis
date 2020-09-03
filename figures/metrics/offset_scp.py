ecaec1fda8d1309dbbae17a3ef5d02c5f35fb225
Reza <radhitya@uwaterloo.ca>
2020-09-03 17:19:50 -0400

    dooot

--- figures/metrics/offset_scp.py
+++ figures/metrics/offset_scp.py
@@ -16,7 +16,7 @@ import statistics as stat
 
 d_gap = 8;
 
-filename5 = 'C://Users//azer//workspace//uw_thesis//figures//metrics//boxes_x//dist_4.csv';
+filename5 = 'C://Users//azer//workspace//uw_thesis//figures//metrics//unilever//dist_4.csv';
 print filename5;
 csv_data5 = pd.read_csv(filename5, sep=',',header=None);
 neg_area_vals_5 = csv_data5.values[:,0];
@@ -30,8 +30,8 @@ for i in range(0, len(neg_area_vals_5)):
 
 ## HISTOGRA< HERE
 
-h_gap = 0.01;
-r_vals = np.arange(h_gap, 50, h_gap);
+h_gap = 0.05;
+r_vals = np.arange(h_gap, 20, h_gap);
 r_vals = np.insert(r_vals, 0, 0);
 
 green_array = negative_list_wrong;
@@ -39,9 +39,13 @@ green_array = [x for x in green_array if x >= 1e-5];
 green_idx = r_vals[:len(green_array)];
 #green_array_grad = np.abs( np.gradient(green_array) );
 
+
+
 plt.clf()
-#plt.plot(green_idx, np.asarray(green_array), 'b', linewidth=1);
-#plt.plot(green_array);
-plt.plot([d_gap, d_gap], [0, 0.5], 'k--')
-plt.scatter(green_idx, green_array, color = 'rgb')
+plt.xlim([0, max(green_idx)])
+plt.ylim([0, max(green_array)])
+plt.plot(green_idx, np.asarray(green_array), 'b', linewidth=1);
+plt.plot([3, 3], [0, 0.5], 'k--')
+plt.plot([5, 5], [0, 0.5], 'k--')
+#plt.scatter(green_idx, green_array, color = 'rgb')
 plt.show();
